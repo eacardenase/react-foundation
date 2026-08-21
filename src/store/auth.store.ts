6fdd54b2e2f1d06db2314f891c5d3e12dd1c493d
Edwin Cardenas <eacardenase@gmail.com>
2026-08-20 20:08:15 -0500

    feat: use and modify stored state

--- src/store/auth.store.ts
+++ src/store/auth.store.ts
@@ -7,10 +7,31 @@ interface AuthState {
         name: string;
         email: string;
     };
+
+    login: (email: string, password: string) => void;
+    logout: () => void;
 }
 
 export const useAuthStore = create<AuthState>()((set) => ({
     status: "checking",
     token: undefined,
     user: undefined,
+
+    login: (email: string, password: string) => {
+        set({
+            status: "authenticated",
+            token: "abc123",
+            user: {
+                name: "John Doe",
+                email: email,
+            },
+        });
+    },
+    logout: () => {
+        set({
+            status: "unauthenticated",
+            token: undefined,
+            user: undefined,
+        });
+    },
 }));
